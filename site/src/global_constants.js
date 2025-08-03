f0e0582af36bcdc71371edfa825bce02049ffcf2
tomheatwole <tgh8xn@virginia.edu>
2025-08-03 14:44:36 -0400

    Add basic "consts" file

---
+++ site/src/global_constants.js
@@ -0,0 +1,6 @@
+// Global constants for use throughout the app
+
+export const SITE_NAME = '<NAME_OF_SITE>';
+export const SITE_LOGO = '<LOGO_URL>';
+export const SITE_FAVICON = '<FAVICON_URL>';
+export const LEAGUE_ID = '<LEAGUE_ID>';
