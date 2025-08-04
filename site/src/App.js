f88e8fcac27851e1ee1a998459895d58237e1327
tomheatwole <tgh8xn@virginia.edu>
2025-08-03 21:57:22 -0400

    Add concept of /team/:id

--- site/src/App.js
+++ site/src/App.js
@@ -1,14 +1,40 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useParams,
+  Navigate
+} from 'react-router-dom';
 import './App.css';
+
+function TeamPage() {
+  const { id } = useParams();
+  return  <h1>Hello, team ID: {id}</h1>;
+}
+
+function HomePage() {
+  return(
+    <header className="App-header">
+      <h1>Welcome to the Hwang Dynasty</h1>
+    </header>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <div className="watermark-bg" />
-      <header className="App-header">
-        <h1>Welcome to the Hwang Dynasty</h1>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div className="content-wrapper">
+        <div className="watermark-bg" />
+          <Routes>
+            <Route path="/team/:id" element={<TeamPage />} />
+            <Route path="/home/" element={<HomePage />} />
+            <Route path="*" element={<Navigate to="/home/" replace />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
