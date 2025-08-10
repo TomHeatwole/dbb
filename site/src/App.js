af4310910b7e4dcfa8ad32ea37d63ab1961bb06f
tomheatwole <tgh8xn@virginia.edu>
2025-08-10 09:20:33 -0700

    More incremental progress towards mobile support

--- site/src/App.js
+++ site/src/App.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate
 } from 'react-router-dom';
+
 import './App.css';
 import TeamPage from './TeamPage';
 import HomePage from './HomePage';
 import Sidebar from './Sidebar';
 
+function useIsMobile(maxWidth = 800) {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= maxWidth);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth <= maxWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [maxWidth]);
+
+  return isMobile;
+}
+
 function App() {
+  const isMobile = useIsMobile({ maxWidth: 800 });
+  const routes = (
+    <Routes>
+      <Route path="/team/:id" element={<TeamPage />} />
+      <Route path="/home/" element={<HomePage />} />
+      <Route path="*" element={<Navigate to="/home/" replace />} />
+    </Routes>
+  );
+
   return (
     <Router>
       <div className="App">
@@ -19,11 +41,7 @@ function App() {
           <Sidebar />
           <div className="main-content">
             <div className="watermark-bg" />
-            <Routes>
-              <Route path="/team/:id" element={<TeamPage />} />
-              <Route path="/home/" element={<HomePage />} />
-              <Route path="*" element={<Navigate to="/home/" replace />} />
-            </Routes>
+            {isMobile ? <div className="mobile-scale-container">{routes}</div> : routes}
           </div>
         </div>
       </div>
