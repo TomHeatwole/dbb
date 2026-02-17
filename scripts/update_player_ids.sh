10adb176043f49c11a78141e7d1edf2e387addf0
tomheatwole <tgh8xn@virginia.edu>
2026-02-17 15:06:34 -0500

    operational maintenance

--- scripts/update_player_ids.sh
+++ scripts/update_player_ids.sh
@@ -1,1 +1,2 @@
 curl -k "https://raw.githubusercontent.com/mayscopeland/ffb_ids/refs/heads/main/player_ids.csv" -o site/public/data/player_ids.txt
+curl -k "https://github.com/nflverse/nflverse-data/releases/download/players/players.csv" -L -o site/public/data/players_gsis_mapping.csv
