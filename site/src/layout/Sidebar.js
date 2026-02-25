04432886d1c37f5fd01c5faaeb13b9802de56c96
tomheatwole <tgh8xn@virginia.edu>
2026-02-25 09:15:47 -0700

    Remove sidebar features

--- site/src/layout/Sidebar.js
+++ site/src/layout/Sidebar.js
@@ -73,8 +73,6 @@ function Sidebar() {
               <li><Link to="/standings">Standings</Link></li>
               <li><Link to="/h2h">Head to Head</Link></li>
               <li><Link to="/yoffs">Playoffs</Link></li>
-              <li><Link to="/trades">Trades</Link></li>
-              <li><Link to="/hwangai">HwangAI</Link></li>
               <li><a href={PODCAST_LINK} target="_blank" rel="noopener noreferrer">Podcast</a></li>
               <li>
                 <div
